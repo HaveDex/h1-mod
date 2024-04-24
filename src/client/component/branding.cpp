2af9910674c0b55cbf28a7124d956bf8576e2f0a
HaveDex <60361277+HaveDex@users.noreply.github.com>
2024-04-24 16:49:41 +0200

    Update branding.cpp

--- src/client/component/branding.cpp
+++ src/client/component/branding.cpp
@@ -42,9 +42,9 @@ namespace branding
 			}
 
 #ifdef DEBUG
-			const auto text = "h1-mod: " VERSION " (" __DATE__ " " __TIME__ ")";
+			const auto text = ""  " ("")";
 #else
-			const auto text = "h1-mod: " VERSION;
+			const auto text = "" ;
 #endif
 
 			const auto placement = game::ScrPlace_GetViewPlacement();
